ba50fed862feea425e99e2a776fe371d70e0d72e
Divesh Thapa <66884874+diveshthapa@users.noreply.github.com>
2024-08-06 17:54:05 +0545

    source

---
+++ portfolio/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Portfolio is coming soon!.
+        </p>
+       
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
